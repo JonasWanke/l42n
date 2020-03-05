4aee33333b724f950ce5d3feaeec2be418691417
Marcel Garus <marcel.garus@gmail.com>
2020-03-05 22:25:11 +0100

    add TranslationField

---
+++ lib/translation_field.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class TranslationField extends StatefulWidget {
+  @override
+  _TranslationFieldState createState() => _TranslationFieldState();
+}
+
+class _TranslationFieldState extends State<TranslationField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
