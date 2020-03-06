10dfe0ffdbafd843469e96d5fa503f6ec4462eed
Jonas Wanke <contact@jonas-wanke.com>
2020-03-06 10:10:34 +0100

    feat: add search

--- lib/editor_page.dart
+++ lib/editor_page.dart
@@ -4,15 +4,22 @@ import 'package:flutter/material.dart';
 
 import 'bloc.dart';
 
-class EditorPage extends StatelessWidget {
+class EditorPage extends StatefulWidget {
   const EditorPage(this.directory) : assert(directory != null);
 
   final Directory directory;
 
   @override
+  _EditorPageState createState() => _EditorPageState();
+}
+
+class _EditorPageState extends State<EditorPage> {
+  String _filter = '';
+
+  @override
   Widget build(BuildContext context) {
     return FutureBuilder<Bloc>(
-      future: Bloc.from(directory),
+      future: Bloc.from(widget.directory),
       builder: (_, snapshot) {
         if (!snapshot.hasData) {
           return Center(
@@ -23,47 +30,81 @@ class EditorPage extends StatelessWidget {
         }
 
         final bloc = snapshot.data;
-        final ids = bloc.ids.toList()..sort();
-
         return Scaffold(
-          appBar: AppBar(
-            title: Text('L42n'),
-          ),
-          body: SingleChildScrollView(
-            child: DataTable(
-              columns: [
-                DataColumn(label: Text('ID')),
-                for (final locale in bloc.locales)
-                  DataColumn(label: Text(locale.toString())),
-              ],
-              rows: [
-                for (final id in ids)
-                  DataRow(
-                    key: ValueKey(id),
-                    cells: [
-                      DataCell(Text(id)),
-                      for (final locale in bloc.locales)
-                        DataCell(
-                          Tooltip(
-                            message:
-                                bloc.strings[id].translations[locale].value ??
-                                    'Not translated yet.',
-                            child: Text(
-                              bloc.strings[id].translations[locale].value ?? '',
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                          placeholder:
-                              bloc.strings[id].translations[locale] == null,
-                        ),
-                    ],
-                  ),
-              ],
-            ),
+          body: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                title: Text('L42n'),
+              ),
+              _buildTopBar(),
+              _buildTable(context, bloc),
+            ],
           ),
         );
       },
+    );
+  }
+
+  Widget _buildTopBar() {
+    return SliverToBoxAdapter(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Flexible(
+            child: FractionallySizedBox(
+              widthFactor: 0.5,
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.search),
+                  labelText: 'Search',
+                ),
+                onChanged: (filter) => setState(() => _filter = filter),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTable(BuildContext context, Bloc bloc) {
+    final ids = bloc.ids
+        .where((id) => id.toLowerCase().contains(_filter.toLowerCase()))
+        .toList()
+          ..sort();
+
+    return SliverToBoxAdapter(
+      child: DataTable(
+        columns: [
+          DataColumn(label: Text('ID')),
+          for (final locale in bloc.locales)
+            DataColumn(label: Text(locale.toString())),
+        ],
+        rows: [
+          for (final id in ids)
+            DataRow(
+              key: ValueKey(id),
+              cells: [
+                DataCell(Text(id)),
+                for (final locale in bloc.locales)
+                  DataCell(
+                    Tooltip(
+                      message: bloc.strings[id].translations[locale].value ??
+                          'Not translated yet.',
+                      child: Text(
+                        bloc.strings[id].translations[locale].value ?? '',
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    placeholder: bloc.strings[id].translations[locale] == null,
+                  ),
+              ],
+            ),
+        ],
+      ),
     );
   }
 }
